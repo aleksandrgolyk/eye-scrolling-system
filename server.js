aa6829c0b78128648c43b30eba1381a3933424b2
Aleksandr <aleksandr.golyk@gmail.com>
2020-11-11 18:10:27 +0200

    added express

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 var app = express()
 
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
 app.listen(3012, function () {
     console.log('API app started')
 })
