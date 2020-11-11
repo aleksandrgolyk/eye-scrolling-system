a7c9d126d6b1cf116c86bb580b592c679005384f
Aleksandr <aleksandr.golyk@gmail.com>
2020-11-11 18:27:41 +0200

    fixed PORT bug

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(function () {
-    // console.log('API app started')
+app.listen(Process, function () {
+    console.log('API app started')
 })
